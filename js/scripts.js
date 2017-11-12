6d97e9326e0e5d0cb147aabbc8e108832cc52d21
MJ <mdjacksonvideo@gmail.com>
2017-11-11 19:17:48 -0800

    create initial HTML for project.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var pingPong = function(number) {
+  if (number % 15 === 0) {
+    return "pingPong";
+  } else  if (number % 5 === 0) {
+    return "pong";
+  } else  if (number % 3 === 0) {
+    return "ping";
+  } else {
+    return number;
+  }
+};
+$(document).ready(function() {
+  $('.btn_createList').click(function(){
+    event.preventDefault();
+    //debugger;
+    var number = parseInt($("input#number").val());
+    var result = pingPong(number);
+    var li_result = "<li>" + result + "</li>"
+      $('.ul_result').append(li_result);
+  });
+});
